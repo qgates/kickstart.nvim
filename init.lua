dc55f7c034c08289bdb65adc2252b0ae3e391604
Seb Tomasini <sebt@qgates.com>
2024-07-25 17:12:41 +0100

    Update which-key config to v3, remove cmp-path, add harpoon bindings

--- init.lua
+++ init.lua
@@ -300,19 +300,15 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>h', desc = 'Git [H]unk', mode = { 'n', 'v' } },
       }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
     end,
   },
 
@@ -732,7 +728,7 @@ require('lazy').setup({
       --  nvim-cmp does not ship with all sources by default. They are split
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
+      -- 'hrsh7th/cmp-path',
     },
     config = function()
       -- See `:help cmp`
@@ -946,10 +942,10 @@ require('lazy').setup({
       }
       vim.keymap.set('n', '<leader>hh', function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
-      end)
+      end, { desc = 'Harpoon s[h]ow list' })
       vim.keymap.set('n', '<leader>ha', function()
         harpoon:list():add()
-      end)
+      end, { desc = 'Harpoon [a]dd buffer' })
       vim.keymap.set('n', '<c-h>', function()
         harpoon:list():select(1)
       end)
@@ -964,16 +960,16 @@ require('lazy').setup({
       end)
       vim.keymap.set('n', '<c-g>5', function()
         harpoon:list():select(5)
-      end)
+      end, { desc = 'Harpoon buffer [5]' })
       vim.keymap.set('n', '<c-g>6', function()
         harpoon:list():select(6)
-      end)
+      end, { desc = 'Harpoon buffer [6]' })
       vim.keymap.set('n', '<c-g>7', function()
         harpoon:list():select(7)
-      end)
+      end, { desc = 'Harpoon buffer [7]' })
       vim.keymap.set('n', '<c-g>8', function()
         harpoon:list():select(8)
-      end)
+      end, { desc = 'Harpoon buffer [8]' })
       vim.keymap.set('n', '<m-j>', function()
         harpoon:list():prev { ui_nav_wrap = true }
       end)
